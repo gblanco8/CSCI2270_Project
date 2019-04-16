bfee8154225fa3e6e1b780b3887253558edb14e5
jbuenviaje <49689553+jbuenviaje@users.noreply.github.com>
2019-04-16 09:52:28 -0600

    Add files via upload

---
+++ Project.cpp
@@ -0,0 +1,37 @@
+#include "Project.hpp"
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+ToDoList::ToDoList(){
+  ToDoList = new Categories[maxQueueSize];
+  
+}
+ToDoList::~ToDoList(){
+
+}
+void ToDoList::addTask(){
+
+}
+void ToDoList::completeTask(){
+
+}
+Profile ToDoList::peek(){
+
+}
+bool ToDoList::isEmpty(){
+
+}
+void ToDoList::printList(){
+
+}
+void ToDoList::search(){
+
+}
+void ToDoList::calculatePriority(){
+
+}
+void ToDoList::repairUpward(int nodeIndex){
+
+}
