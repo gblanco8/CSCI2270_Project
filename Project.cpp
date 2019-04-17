a57849dbafac6f2ed9dd4c74f16a91f9f1ce0ee4
gblanco8 <49006173+gblanco8@users.noreply.github.com>
2019-04-16 22:35:42 -0600

    Update Project.cpp

--- Project.cpp
+++ Project.cpp
@@ -8,12 +8,15 @@ ToDoList::ToDoList(){
   TaskPriority = new Task[maxQueueSize];
   currentQueueSize = 0;
 }
+/////
 ToDoList::~ToDoList(){
   delete [] TaskPriority;
 }
+/////
 void ToDoList::addProfile(){
   
-}  
+}
+/////
 void ToDoList::addTask(){
   Task nT;
   float priority = 0;
@@ -30,6 +33,7 @@ void ToDoList::addTask(){
   repairUpward(currentQueueSize);
   currentQueueSize++;
 }
+/////
 void ToDoList::completeTask(){
   if(!isEmpty()){
     TaskPriority[0] = TaskPriority[currentQueueSize-1];
@@ -40,6 +44,7 @@ void ToDoList::completeTask(){
     cout << "List empty, cannot dequeue" << endl;
   }
 }
+/////
 Profile ToDoList::peek(){
   if(isEmpty()){
     cout << "To do list empty, nothing to see" << endl;
@@ -48,6 +53,7 @@ Profile ToDoList::peek(){
     return TaskPriority[0];
   }
 }
+/////
 bool ToDoList::isEmpty(){
   if(currentQueueSize == 0){
     return true;
@@ -55,11 +61,13 @@ bool ToDoList::isEmpty(){
     return false;
   }
 }
+/////
 void ToDoList::printList(){
   for(int i=0; i<currentQueueSize; i++){
-    cout << TaskPriority[i]->taskName
+    cout << i+1 << ". " << TaskPriority[i]->taskName << endl;
   }
 }
+/////
 int ToDoList::search(string TaskName){                               
   for(int i = 0; i < currentQueueSize; i++){
     if(TaskPriority[i].taskname == TaskName){
@@ -67,14 +75,17 @@ int ToDoList::search(string TaskName){
     }
   }
 }
-void ToDoList::calculatePriority(){
+/////
+double ToDoList::calculatePriority(){
 
 }
+/////
 void swap(Task* a, Task* b){
   Task temp = a;
   a = b;
   b = temp;
 }
+//////
 void ToDoList::repairUpward(int nodeIndex){
   int p = (nodeIndex-1)/2;
 
